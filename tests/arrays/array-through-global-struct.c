3c31c55e865c7f77b5edb079ca017147a0770758
Tristan Ravitch <travitch@cs.wisc.edu>
2012-01-17 17:16:22 -0600

    This test should not pass yet
    It needs a complementary analysis that identifies what fields of
    structures are used as arrays.

--- tests/arrays/array-through-global-struct.c
+++ tests/arrays/array-through-global-struct.c
@@ -1,3 +1,12 @@
+/*
+  I've changed my mind on this test.  Doing a shape analysis to
+  note that s.fld aliases arr1 is too expensive.  Do not catch this
+  at this point in the analysis.
+
+  This can be changed later and be found by noting that field S.fld is
+  used as an array in various points in the application and the
+  assignment of arr1 to s.fld makes arr1 an array.
+ */
 struct S {
   int * fld;
   double d;

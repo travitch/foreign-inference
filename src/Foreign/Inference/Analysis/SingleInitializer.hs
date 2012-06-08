5735efd7d88f0e8ddb5833822116b6a40e0ebb9e
Tristan Ravitch <travitch@cs.wisc.edu>
2012-06-08 11:05:46 -0500

    Major enhancements for the initializer analysis
    It is now a cheap, unsound, incomplete field-sensitive points to
    analysis for function pointers.  It will evolve into a more general
    indirect call resolver.  This isn't a general points-to analysis but
    is very useful for this project.
    
    This change also improves the precision of the analysis in two ways.
    
     1) absPathLookup (lookup initializer for abstract path) now
        looks up the result for a path and adds it to the recursively
        reduced path.
    
        For paths like S.t.foo, this means two lookups:
    
        * S.t.foo and
        * T.foo
    
        This is important for cases where S.t.foo is never directly
        initialized; instead, it might be the case that t.foo is
        initialized somewhere and then S.t is assigned that result.
    
     2) Abstract access paths are now canonicalized before being used.
        Abstract access paths refer to LLVM types to denote their base,
        but often functionally equivalent types cannot be compared for
        equality because the frontends emit strange variants.  Example:
    
        > %vtbl = { i32(i32*)*, i32(i32*)*, i32(i32*, i32*)* }
        > %vtbl.11 { {}*, {}*, i32(i32*, i32*)* }
    
        In this simplified example, these types are identical in the
        source but are emitted in this strange way.  These cannot be
        unified by the LLVM linker (nor in a post-processing step because
        they really are not identical, though their representations are).
        This analysis now unifies types like this by name.  This isn't
        always 100% safe (the types may have really been different in the
        source), but it is fine for our purposes and those cases are very
        rare.  It may be worth changing this post-processing unification
        to require strict physical compatibility.
    
        This canonicalization step just allows simple equality tests and
        assignments across compilation units to be considered.  It noticably
        improves at least libarchive.

--- src/Foreign/Inference/Analysis/SingleInitializer.hs
+++ src/Foreign/Inference/Analysis/SingleInitializer.hs
@@ -29,7 +29,7 @@ module Foreign.Inference.Analysis.SingleInitializer (
   singleInitializer
   ) where
 
-import Data.List ( elemIndex, foldl' )
+import Data.List ( elemIndex, find, foldl', intercalate )
 import Data.List.Ordered ( union )
 import Data.Map ( Map )
 import qualified Data.Map as M
@@ -37,12 +37,35 @@ import Data.Monoid
 
 import LLVM.Analysis
 import LLVM.Analysis.AccessPath
+import LLVM.Analysis.PointsTo
 
 -- import Text.Printf
 -- import Debug.Trace
 -- debug = flip trace
 
--- Also, allow paths in finalizers to include exit-like functions.
+-- FIXME: Assignments of struct pointers with field initializers
+-- to fields of parent structures should propagate initializers.
+--
+-- > vtbl->a = foo;
+-- > vtbl->b = bar;
+-- > obj->vtbl = vtbl;
+--
+-- Asking queries about @obj->vtbl->a@ yields [], when it should yield
+-- [foo].  Adding a dependency between obj.vtbl and vtbl.* would let
+-- us connect these after every assignment is processed and we can
+-- construct these extended results.
+
+-- FIXME: Have this implement the PointsTo analysis interface and then
+-- use it to construct the call graphs for all analyses.  This way
+-- indirect calls can be resolved and functions analyzed in order.
+
+-- FIXME Rename to IndirectCallResolver
+
+-- | This isn't a true points-to analysis because it is an
+-- under-approximation.  However, that is sufficient for this library.
+instance PointsToAnalysis SingleInitializerSummary where
+  mayAlias _ _ _ = True
+  pointsTo = singleInitializer
 
 data SingleInitializerSummary =
   SIS { abstractPathInitializers :: !(Map AbstractAccessPath [Value])
@@ -56,13 +79,64 @@ data SingleInitializerSummary =
         -- Function that is the key of the map.
       , fieldArgDependencies :: !(Map AbstractAccessPath [(Function, Int)])
       , globalArgDependencies :: !(Map GlobalVariable [(Function, Int)])
+      , canonicalTypeMap :: Map Type Type
       }
   deriving (Eq)
 
-instance Monoid SingleInitializerSummary where
-  mempty = SIS mempty mempty mempty mempty mempty
-  mappend (SIS a1 c1 arg1 f1 g1) (SIS a2 c2 arg2 f2 g2) =
-    SIS (a1 `mappend` a2) (c1 `mappend` c2) (arg1 `mappend` arg2) (f1 `mappend` f2) (g1 `mappend` g2)
+instance Show SingleInitializerSummary where
+  show (SIS api cbi _ _ _ _) = concat [ "Abstract Path Initializers\n"
+                                      , unlines $ map showAPI (M.toList api)
+                                      , "\nConcrete Value Initializers\n"
+                                      , unlines $ map showCBI (M.toList cbi)
+                                      ]
+    where
+      showAPI (aap, vs) = concat [ " * ", show aap, ": ", intercalate ", " (map (show . valueName) vs)]
+      showCBI (gv, vs) = concat [ " * ", show (globalVariableName gv), ": ", intercalate ", " (map (show . valueName) vs)]
+
+emptyInitializerSummary :: Module -> Map GlobalVariable [Value] -> SingleInitializerSummary
+emptyInitializerSummary m cvis =
+  SIS mempty cvis mempty mempty mempty (makeCanonicalTypeMap (moduleRetainedTypes m))
+
+makeCanonicalTypeMap :: [Type] -> Map Type Type
+makeCanonicalTypeMap ts =
+  foldr canonicalize mempty (M.toList typesByName)
+  where
+    typesByName = foldr addTypeByName mempty ts
+    addTypeByName t m =
+      case t of
+        TypeStruct _ _ _ ->
+          case structTypeToName t of
+            Nothing -> m
+            Just tn -> M.insertWith' (++) tn [t] m
+        _ -> m
+    canonicalize (_, gts) acc =
+      case find nameHasOneDot gts of
+        Nothing -> acc
+        Just ctype -> foldr (insertCanonical ctype) acc gts
+    insertCanonical ctype t = M.insert t ctype
+    -- Struct names with one dot are of the form struct.name -- they
+    -- have no numeric suffix.  These are the types we are taking as
+    -- canonical
+    nameHasOneDot (TypeStruct (Just n) _ _) = length (filter (=='.') n) == 1
+    nameHasOneDot _ = False
+
+canonicalizeType :: SingleInitializerSummary -> Type -> Type
+canonicalizeType sis ty@(TypeStruct _ _ _) =
+  M.findWithDefault ty ty (canonicalTypeMap sis)
+canonicalizeType sis (TypePointer t' a) =
+  TypePointer (canonicalizeType sis t') a
+canonicalizeType sis (TypeFunction r ts v) =
+  TypeFunction (canonicalizeType sis r) (map (canonicalizeType sis) ts) v
+canonicalizeType _ t = t
+
+canonicalizeAccessPath :: SingleInitializerSummary
+                          -> AbstractAccessPath
+                          -> AbstractAccessPath
+canonicalizeAccessPath s (AbstractAccessPath bt et cs) =
+  AbstractAccessPath { abstractAccessPathBaseType = canonicalizeType s bt
+                     , abstractAccessPathEndType = canonicalizeType s et
+                     , abstractAccessPathComponents = cs
+                     }
 
 singleInitializer :: SingleInitializerSummary -> Value -> [Value]
 singleInitializer s v =
@@ -70,20 +144,31 @@ singleInitializer s v =
     InstructionC i -> maybe [] id $ do
       accPath <- accessPath i
       let absPath = abstractAccessPath accPath
+          cabsPath = canonicalizeAccessPath s absPath
       case valueContent' (accessPathBaseValue accPath) of
         GlobalVariableC gv@GlobalVariable { globalVariableInitializer = Just initVal } ->
-          case followAccessPath absPath initVal of
+          case followAccessPath cabsPath initVal of
             Nothing -> return $! globalVarLookup s gv
             accPathVal -> fmap return accPathVal
-        _ -> return $! absPathLookup s absPath
+        _ -> return $! absPathLookup s cabsPath
     _ -> []
 
+-- | Look up the initializers for this abstract access path.  The key
+-- here is that we get both the initializers we know for this path,
+-- along with initializers for *suffixes* of the path.  For example,
+-- if the path is a.b.c.d, we also care about initializers for b.c.d
+-- (and c.d).  The recursive walk is in the reducedPathResults
+-- segment.
 absPathLookup :: SingleInitializerSummary -> AbstractAccessPath -> [Value]
-absPathLookup s absPath = storeInits `union` argInits
+absPathLookup s absPath = storeInits `union` argInits `union` reducedPathResults
   where
     storeInits = M.findWithDefault [] absPath (abstractPathInitializers s)
     argDeps = M.findWithDefault [] absPath (fieldArgDependencies s)
     argInits = concatMap (\x -> M.findWithDefault [] x (argumentInitializers s)) argDeps
+    reducedPathResults =
+      case reduceAccessPath absPath of
+        Nothing -> []
+        Just rpath -> absPathLookup s rpath
 
 globalVarLookup :: SingleInitializerSummary -> GlobalVariable -> [Value]
 globalVarLookup s gv = concreteInits `union` argInits
@@ -92,11 +177,19 @@ globalVarLookup s gv = concreteInits `union` argInits
     argDeps = M.findWithDefault [] gv (globalArgDependencies s)
     argInits = concatMap (\x -> M.findWithDefault [] x (argumentInitializers s)) argDeps
 
+
+-- canonicalize types in all abstract access paths.  computed AAPs in
+-- the lookup step will also need to canonicalize.  Just assume that
+-- types sharing the same name are all the same and ignore .NNN
+-- variants.
+
+-- | Run the initializer analysis: a cheap pass to identify a subset
+-- of possible function pointers that object fields can point to.
 identifySingleInitializers :: Module -> SingleInitializerSummary
 identifySingleInitializers m =
   foldl' (flip recordInitializers) s0 allInsts
   where
-    s0 = mempty { concreteValueInitializers = M.fromList globalsWithInits }
+    s0 = emptyInitializerSummary m (M.fromList globalsWithInits)
     allBlocks = concatMap functionBody $ moduleDefinedFunctions m
     allInsts = concatMap basicBlockInstructions allBlocks
     globalsWithInits = foldr extractGlobalsWithInits [] (moduleGlobalVariables m)
@@ -169,8 +262,9 @@ recordArgInitializer i f ix sa s =
         Nothing -> s
         Just accPath ->
           let absPath = abstractAccessPath accPath
+              cabsPath = canonicalizeAccessPath s absPath
           in s { fieldArgDependencies =
-                    M.insertWith' union absPath [(f, ix)] (fieldArgDependencies s)
+                    M.insertWith' union cabsPath [(f, ix)] (fieldArgDependencies s)
                }
 
 -- | Initializers here (sv) are only functions (external or otherwise)
@@ -188,6 +282,7 @@ maybeRecordInitializer i sv sa s =
         Nothing -> s
         Just accPath ->
           let absPath = abstractAccessPath accPath
+              cabsPath = canonicalizeAccessPath s absPath
           in s { abstractPathInitializers =
-                    M.insertWith' union absPath [sv] (abstractPathInitializers s)
+                    M.insertWith' union cabsPath [sv] (abstractPathInitializers s)
                }

6e38a3f19c3c343228f22677157b258934538bee
Tristan Ravitch <travitch@cs.wisc.edu>
2012-08-21 17:22:17 -0500

    case -> guards

--- src/Foreign/Inference/Internal/FlattenValue.hs
+++ src/Foreign/Inference/Internal/FlattenValue.hs
@@ -1,6 +1,7 @@
 module Foreign.Inference.Internal.FlattenValue ( flattenValue ) where
 
 import Data.List ( foldl' )
+import Data.HashSet ( HashSet )
 import qualified Data.HashSet as S
 
 import LLVM.Analysis
@@ -10,20 +11,23 @@ import LLVM.Analysis
 -- that phi and select instructions could point to.
 flattenValue :: Value -> [Value]
 flattenValue = S.toList . go S.empty . S.singleton
+
+go :: HashSet Value -> HashSet Value -> HashSet Value
+go visited q
+  | S.null vals = visited
+  | otherwise =
+    let visited' = visited `S.union` vals
+        q' = foldl' addValuesFrom S.empty (S.toList vals)
+    in go visited' q'
   where
-    go visited q =
-      let vals = S.difference q visited
-      in case S.null vals of
-        True -> visited
-        False ->
-          let visited' = visited `S.union` vals
-              q' = foldl' addValuesFrom S.empty (S.toList vals)
-          in go visited' q'
-    addValuesFrom q v =
-      case valueContent' v of
-        InstructionC PhiNode { phiIncomingValues = pvs } ->
-          let vs = map fst pvs
-          in foldr S.insert q vs
-        InstructionC SelectInst { selectTrueValue = tv, selectFalseValue = fv } ->
-          foldr S.insert q [tv, fv]
-        _ -> S.insert v $ S.insert (stripBitcasts v) q
+    vals = S.difference q visited
+
+addValuesFrom :: HashSet Value -> Value -> HashSet Value
+addValuesFrom q v =
+  case valueContent' v of
+    InstructionC PhiNode { phiIncomingValues = pvs } ->
+      let vs = map fst pvs
+      in foldr S.insert q vs
+    InstructionC SelectInst { selectTrueValue = tv, selectFalseValue = fv } ->
+      foldr S.insert q [tv, fv]
+    _ -> S.insert v $ S.insert (stripBitcasts v) q

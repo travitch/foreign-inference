c504810fed7fed12e244aa3a1ded50c5c4308e3a
Tristan Ravitch <travitch@cs.wisc.edu>
2012-10-21 15:40:50 -0500

    Remove the obsoleted indirect call resolver code
    Also restrict the andersens analysis to only fptr types

--- src/Foreign/Inference/Analysis/IndirectCallResolver.hs
+++ src/Foreign/Inference/Analysis/IndirectCallResolver.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE ViewPatterns, OverloadedStrings, FlexibleContexts #-}
+{-# LANGUAGE ViewPatterns #-}
 -- | FIXME: Currently there is an exception allowing us to identify
 -- finalizers that are called through function pointers if the
 -- function pointer is global and has an initializer.
@@ -30,21 +30,7 @@ module Foreign.Inference.Analysis.IndirectCallResolver (
   indirectCallTargets
   ) where
 
-import Control.Exception ( throw )
-import Control.Monad ( forM_ )
-import Data.Hashable
-import Data.HashMap.Strict ( HashMap )
-import qualified Data.HashMap.Strict as HM
-import Data.HashSet ( HashSet )
-import qualified Data.HashSet as HS
-import Data.List ( elemIndex )
-import Data.Maybe ( fromMaybe )
-import Data.Monoid
-
--- import Database.Datalog
-
 import LLVM.Analysis
-import LLVM.Analysis.AccessPath
 import LLVM.Analysis.ClassHierarchy
 import LLVM.Analysis.PointsTo
 import LLVM.Analysis.PointsTo.Andersen
@@ -60,26 +46,10 @@ instance PointsToAnalysis IndirectCallSummary where
   pointsTo = indirectCallInitializers
   resolveIndirectCall = indirectCallTargets
 
--- data CallSummary = ArgumentPosition Function Int
---                    -- ^ For describing call argument positions and
---                    -- formal positions
---                  | Path AbstractAccessPath
---                    -- ^ Paths that are assigned to
---                  | Target Value
---                  deriving (Eq, Ord, Show)
-
--- instance Hashable CallSummary where
---   hash (ArgumentPosition f i) = 1 `combine` hash f `combine` hash i
---   hash (Path p) = 2 `combine` hash p
---   hash (Target f) = 3 `combine` hash f
-
 data IndirectCallSummary =
-  ICS { summaryTargets :: Andersen -- HashMap AbstractAccessPath (HashSet Value)
+  ICS { summaryTargets :: Andersen
       , resolverCHA :: CHA
       }
-
--- instance Show IndirectCallSummary where
---   show = show . summaryTargets
 
 -- If i is a Load of a global with an initializer (or a GEP of a
 -- global with a complex initializer), just use the initializer to
@@ -88,20 +58,20 @@ data IndirectCallSummary =
 -- Eventually, this should call down to a real (field-based) points-to
 -- analysis for other values.
 indirectCallInitializers :: IndirectCallSummary -> Value -> [Value]
-indirectCallInitializers s@(ICS pta _) v =
+indirectCallInitializers ics v =
   case valueContent' v of
     -- Here, walk the initializer if it isn't a simple integer
     -- constant We discard the first index because while the global
     -- variable is a pointer type, the initializer is not (because all
     -- globals get an extra indirection as r-values)
-    InstructionC li@LoadInst { loadAddress = la@(valueContent' ->
+    InstructionC li@LoadInst { loadAddress = (valueContent' ->
       ConstantC ConstantValue { constantInstruction = (valueContent' ->
         InstructionC GetElementPtrInst { getElementPtrValue = (valueContent' ->
           GlobalVariableC GlobalVariable { globalVariableInitializer = Just i })
                                        , getElementPtrIndices = (valueContent -> ConstantC ConstantInt { constantIntValue = 0 }) :ixs
                                        })})} ->
       maybe (lookupInst li) (:[]) $ resolveInitializer i ixs
-    InstructionC li@LoadInst { loadAddress = la@(valueContent' ->
+    InstructionC li@LoadInst { loadAddress = (valueContent' ->
       GlobalVariableC GlobalVariable { globalVariableInitializer = Just i })} ->
       case valueContent' i of
         -- All globals have some kind of initializer; if it is a zero
@@ -109,16 +79,12 @@ indirectCallInitializers s@(ICS pta _) v =
         -- use the more complex fallback.
         ConstantC _ -> lookupInst li
         _ -> [i]
-    InstructionC li@LoadInst { loadAddress = la } ->
+    InstructionC li@LoadInst { } ->
       lookupInst li
     InstructionC i -> lookupInst i
     _ -> []
   where
-    lookupInst i = pointsTo pta (toValue i)
-    -- lookupInst i = fromMaybe [] $ do
-    --   accPath <- accessPath i
-    --   let absPath = abstractAccessPath accPath
-    --   return $! indirectCallLookup s absPath
+    lookupInst i = pointsTo (summaryTargets ics) (toValue i)
 
 resolveInitializer :: Value -> [Value] -> Maybe Value
 resolveInitializer v [] = return (stripBitcasts v)
@@ -137,9 +103,6 @@ fromConstantInt v =
     ConstantC ConstantInt { constantIntValue = iv } ->
       return $ fromIntegral iv
     _ -> Nothing
-
--- indirectCallLookup :: IndirectCallSummary -> AbstractAccessPath -> [Value]
--- indirectCallLookup s = HS.toList . absPathLookup s
 
 -- | Resolve the targets of an indirect call instruction.  This works
 -- with both C++ virtual function dispatch and some other common
@@ -160,175 +123,15 @@ indirectCallTargets ics i =
           indirectCallInitializers ics f
         _ -> []
 
--- | This is the datalog program to compute a simple transitive
--- closure (and unify function actual arguments with formals).  Note
--- that it can't be as polymorphic as certain other datalog functions
--- since it takes no arguments.
--- pathQuery :: QueryBuilder (Either DatalogError) CallSummary (Query CallSummary)
--- pathQuery = do
---   fptrToField <- relationPredicateFromName "fptrToField"
---   fptrAsArg <- relationPredicateFromName "fptrAsArg"
---   argToField <- relationPredicateFromName "argToField"
---   initializes <- inferencePredicate "initializes"
-
---   let f = LogicVar "F"
---       p = LogicVar "P"
---       pos = LogicVar "POS"
-
---   (initializes, [f, p]) |- [ lit fptrToField [ f, p ] ]
---   (initializes, [f, p]) |- [ lit fptrAsArg [ pos, f ]
---                            , lit argToField [ pos, p ]
---                            ]
---   issueQuery initializes [ f, p ]
-
--- | Look up the initializers for this abstract access path.  The key
--- here is that we get both the initializers we know for this path,
--- along with initializers for *suffixes* of the path.  For example,
--- if the path is a.b.c.d, we also care about initializers for b.c.d
--- (and c.d).  The recursive walk is in the reducedPathResults
--- segment.
--- absPathLookup :: IndirectCallSummary -> AbstractAccessPath -> HashSet Value
--- absPathLookup s@(ICS targets _) absPath =
---   pathInits `HS.union` reducedPathResults
---   where
---     pathInits = HM.lookupDefault mempty absPath targets
---     reducedPathResults =
---       case reduceAccessPath absPath of
---         Nothing -> mempty
---         Just rpath -> absPathLookup s rpath
-
 -- | Run the initializer analysis: a cheap pass to identify a subset
 -- of possible function pointers that object fields can point to.
-{-# NOINLINE identifyIndirectCallTargets #-}
 identifyIndirectCallTargets :: Module -> IndirectCallSummary
 identifyIndirectCallTargets m =
   ICS (runPointsToAnalysisWith ignoreNonFptr m) (runCHA m)
   where
-    ignoreNonFptr = const False -- ignoreNonFptrType . valueType
+    ignoreNonFptr = ignoreNonFptrType . valueType
     ignoreNonFptrType t =
       case t of
         TypeFunction _ _ _ -> False
         TypePointer t' _ -> ignoreNonFptrType t'
         _ -> True
-
-  -- where
-  --   facts = either throw id $ do
-  --     db <- buildDatabase m
-  --     queryDatabase db pathQuery
-  --   targets = foldr addTarget mempty facts
-  --   addTarget [Target f, Path p] = HM.insertWith HS.union p (HS.singleton f)
-  --   addTarget _ = error "identifyIndirectCallTargets: database schema mismatch"
-{-
-
-buildDatabase :: Module -> Either DatalogError (Database CallSummary)
-buildDatabase m = makeDatabase $ do
-  fptrToField <- addRelation "fptrToField" 2
-  fptrAsArg <- addRelation "fptrAsArg" 2
-  argToField <- addRelation "argToField" 2
-  mapM_ (factsForGlobal fptrToField) (moduleGlobalVariables m)
-  let f = mapM_ (factsForInstruction fptrToField fptrAsArg argToField) . functionInstructions
-  mapM_ f (moduleDefinedFunctions m)
-
--- | Collect facts from global variable initializers.  It currently
--- only handles initializers assigned to global function pointers and
--- global structure initializers.  It does not yet handle nested
--- global initializers.
-factsForGlobal :: (Failure DatalogError m)
-                  => Relation
-                  -> GlobalVariable
-                  -> DatabaseBuilder m CallSummary ()
-factsForGlobal _ GlobalVariable { globalVariableInitializer = Nothing } = return ()
-factsForGlobal fptrToField gv@GlobalVariable { globalVariableInitializer = Just i } =
-  case valueContent' i of
-    FunctionC f -> addPlainInitializer (toValue f)
-    ExternalFunctionC f -> addPlainInitializer (toValue f)
-    ConstantC (ConstantStruct _ _ is) ->
-      forM_ (zip [0..] is) $ \(idx, initializer) ->
-        case valueContent' initializer of
-          FunctionC f -> addAggregateInitializer idx (toValue f)
-          ExternalFunctionC f -> addAggregateInitializer idx (toValue f)
-          _ -> return ()
-    _ -> return ()
-  where
-    addPlainInitializer v =
-      let bt = valueType gv
-          cs = [AccessDeref]
-          p = AbstractAccessPath bt bt cs
-      in assertFact fptrToField [ Target v, Path p ]
-    addAggregateInitializer idx v =
-      let bt = valueType gv
-          et = TypePointer (TypePointer (valueType v) 0) 0
-          cs = [AccessDeref, AccessField idx]
-          p = AbstractAccessPath bt et cs
-      in assertFact fptrToField [ Target v, Path p]
-
--- | Record initializers provided by assignments and function calls in
--- the actual code.
-factsForInstruction :: (Failure DatalogError m)
-                       => Relation
-                       -> Relation
-                       -> Relation
-                       -> Instruction
-                       -> DatabaseBuilder m CallSummary ()
-factsForInstruction fptrToField fptrAsArg argToField i =
-  case i of
-    StoreInst { storeValue = sv, storeAddress = sa } ->
-      case valueContent' sv of
-        FunctionC f -> addStoredFunc (toValue f)
-        ExternalFunctionC ef -> addStoredFunc (toValue ef)
-        ArgumentC a ->
-          case accessPath i of
-            Nothing -> return ()
-            Just accPath -> do
-              let f = argumentFunction a
-                  Just ix = elemIndex a (functionParameters f)
-                  absPath = abstractAccessPath accPath
-              addSubFacts (\p -> assertFact argToField [ArgumentPosition f ix, Path p]) absPath
-        _ -> return ()
-    CallInst { callFunction = (valueContent' -> FunctionC f)
-             , callArguments = args
-             } ->
-      mapM_ (argPosFacts f) (zip [0..] (map fst args))
-    InvokeInst { invokeFunction = (valueContent' -> FunctionC f)
-               , invokeArguments = args
-               } ->
-      mapM_ (argPosFacts f) (zip [0..] (map fst args))
-    _ -> return ()
-  where
-    -- Add facts for all sub-fields of an assignment.  Example for
-    --
-    -- > a->b->c->d = foo
-    --
-    -- adds facts:
-    --
-    -- > a->b->c->d = foo
-    -- > b->c->d = foo
-    -- > c->d = foo
-    --
-    -- Note, we don't add facts for just d or the empty path.
-    addSubFacts func p = do
-      _ <- func p
-      case reduceAccessPath p of
-        Nothing -> return ()
-        Just p' ->
-          case any someField (abstractAccessPathComponents p') of
-            False -> return ()
-            True -> addSubFacts func p'
-
-    addStoredFunc v =
-      case accessPath i of
-        Nothing -> return ()
-        Just accPath -> do
-          let absPath = abstractAccessPath accPath
-          addSubFacts (\p -> assertFact fptrToField [Target v, Path p]) absPath
-
-    argPosFacts f (ix, val) =
-      case valueContent' val of
-        FunctionC fptr ->
-          assertFact fptrAsArg [ ArgumentPosition f ix, Target (toValue fptr) ]
-        ExternalFunctionC fptr ->
-          assertFact fptrAsArg [ ArgumentPosition f ix, Target (toValue fptr) ]
-        _ -> return ()
-    someField (AccessField _) = True
-    someField _ = False
--}
